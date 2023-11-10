8d161ad0c3a725e3bf26e2d724bf39f7755a6345
yangxiao <yangxiao0303@gmail.com>
2023-11-10 13:01:54 +0800

    02_静态以及全局组件的搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+//引入vue2核心库
 import Vue from 'vue'
+//引入根组件:单文件组件.vue
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+// 创建全局组件
+Vue.component("Header",Header);
+Vue.component("Footer",Footer);
+// 引入全局清空样式
+import '@/assets/reset.css'
+//创建vm
 new Vue({
   render: h => h(App),
 }).$mount('#app')
