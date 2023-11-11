a1f5916e3706972a3da8297fc33388735b717106
yangxiao <yangxiao0303@gmail.com>
2023-11-11 15:10:53 +0800

    06_配置跨域请求代理

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  //https://webpack.docschina.org/configuration/dev-server/#devserverproxy
+  // 配置代理跨域
+  devServer:{
+    proxy:{
+      '/api':{
+        // 设置目标服务器地址
+        target:'http://sph-h5-api.atguigu.cn',
+        // 查看路径上有没有'/api'这个路径,如果有.要添加 路径重写配置
+        
+      }
+    }
+  }
 })
