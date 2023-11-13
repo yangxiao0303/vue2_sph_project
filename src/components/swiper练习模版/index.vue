5ea35e64b49cbfb771a7925680607061649f0c91
yangxiao <yangxiao0303@gmail.com>
2023-11-13 18:42:19 +0800

    12_vue2-awesome-swiper使用练习

---
+++ src/components/swiper练习模版/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <Swiper class="swiper" :options="SwiperOptions">
+    <SwiperSlide v-for="item in 5" :key="item">
+      <!-- 具名插槽 -->
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </SwiperSlide>
+  </Swiper>
+</template>
+
+<script>
+  export default {
+    name:'Swiper',
+    data() {
+      return {
+        SwiperOptions:{
+          navigation : {
+            // 导航摁钮
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next"
+          },
+          loop: true, // 无线循环
+          direction: "horizontal", // 水平方向
+          pagination: {
+            el: ".swiper-pagination", // 分页小球
+            clickable: true // 是否可点击
+          },
+          autoplay: {
+            delay: 1000, // 每页停留时间
+            stopOnLastSlide: false, // 最后一页停留
+            disableOnInteraction: true, // 交互时停止自动播放
+          }
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped lang='less'>
+  
+</style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import App from './App.vue'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 import TypeNav from '@/components/typenav'
+import { Swiper,SwiperSlide } from 'vue-awesome-swiper'
+// 引入 Swiper,SwiperSlide 样式
+import "swiper/css/swiper.min.css"
 // 放置全局组件
 Vue.component("Header",Header);
 Vue.component("Footer",Footer);
 Vue.component("TypeNav",TypeNav);
+Vue.component("Swiper",Swiper);
+Vue.component("SwiperSlide",SwiperSlide);
 // 引入全局清空样式
 import '@/assets/reset.css'
 // 引入配置好的路由规则
