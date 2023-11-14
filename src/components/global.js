ab3b1d201602b3be191661577d640e5fdc2db104
yangxiao <yangxiao0303@gmail.com>
2023-11-14 17:03:26 +0800

    15_自定义插件

---
+++ src/components/global.js
@@ -0,0 +1,28 @@
+// 引入全局组件
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import TypeNav from "@/components/typenav";
+// 放置全局组件
+// Vue.component("Header",Header);
+// Vue.component("Footer",Footer);
+// Vue.component("TypeNav",TypeNav);
+
+const globalObj = {
+  Header,
+  Footer,
+  TypeNav
+}
+
+// 自定义插件对象
+export default {
+  // 在 Vue.use 安装插件时,会调用插件对象的install方法一次,注入构造函数Vue
+  install(Vue){
+    // 用Object.keys方法获取由键名组成的数组
+    Object.keys(globalObj).forEach(component => {
+      // 将每个组件都挂载为全局组件
+      /* 注意,此处第二参数不能用 .component,
+       因为 .属性命名方式不能为参数,晶哥讲过 */ 
+      Vue.component(component,globalObj[component]);
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@
 import Vue from 'vue'
 //引入根组件:单文件组件.vue
 import App from './App.vue'
-// 引入全局组件
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-import TypeNav from '@/components/typenav'
+// 引入 vue-awesome-swiper 
 import { Swiper,SwiperSlide } from 'vue-awesome-swiper'
 // 引入 Swiper,SwiperSlide 样式
 import "swiper/css/swiper.min.css"
-// 放置全局组件
-Vue.component("Header",Header);
-Vue.component("Footer",Footer);
-Vue.component("TypeNav",TypeNav);
+// 放置 轮播图组件
 Vue.component("Swiper",Swiper);
 Vue.component("SwiperSlide",SwiperSlide);
+// 引入全局组件
+import global from '@/components/global';
+// 安装使用自定义插件
+Vue.use(global);
 // 引入全局清空样式
 import '@/assets/reset.css'
 // 引入配置好的路由规则
