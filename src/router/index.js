8c9beca98e8e244f72176d396bd171599257866b
yangxiao <yangxiao0303@gmail.com>
2023-11-14 15:32:47 +0800

    14_编程式导航的重写

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,24 @@ import Search from '@/pages/search';
 import Login from '@/pages/login';
 import Register from '@/pages/register';
 
+// 重写编程式导航的push | replace 方法 以解决多次点击报错的问题
+// 注意: 如防止报错push | replace 方法 的 this 必须指向 VueRouter的实例化对象
+
+// 存储原本的 push和 replace 方法
+const push = VueRouter.prototype.push;
+const replace = VueRouter.prototype.replace;
+
+// 自定义方法
+VueRouter.prototype.push = function(loaction){
+  // apply的第一个参数为修改的指向
+  push.apply(this,[loaction, ()=>{}, ()=>{}]);
+};
+
+VueRouter.prototype.replace = function(loaction){
+  // call的第一个参数为修该过后的指向
+  replace.push(this,()=>{},()=>{});
+}
+
 // 配置路由并暴露
 export default new VueRouter({
   // 模式 可以不配置
